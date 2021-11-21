075768c523644294cf3e830a961bc9e568f03370
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-11-21 15:53:08 -0300

    Middleware Verify if user is logged in

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { join } = require("path");
+const middleware = require("./middleware");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (request, response) => {
-  response.status(200).send("Hello Guys!");
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+
+app.get("/", middleware.requireLogin, (request, response) => {
+  const payLoad = { title: "Home" };
+  response.status(200).render("home", payLoad);
 });
 
 app.listen(PORT || 8080, () => {
---
+++ middleware.js
@@ -0,0 +1,5 @@
+exports.requireLogin = (request, response, next) => {
+  if (!request.session && !request.session?.user)
+    return response.redirect("/login");
+  next();
+};
