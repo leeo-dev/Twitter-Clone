7907db634b2970976516420a9aa1b9c5f4bf2e4a
Leonardo Albuquerque <37962036+leeodesign@users.noreply.github.com>
2021-11-21 15:09:16 -0300

    Project Set up

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (request, response) => {
+  response.status(200).send("Hello Guys");
+});
+
+app.listen(PORT || 8080, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
